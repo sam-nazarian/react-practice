41b4fd9f0c1258a4328721938c36f8a32c3be0fd
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-02 17:13:28 -0600

    Implemented Section 3 - Part 1, practiced with react props, components & jsx

---
+++ Section-3/src/Components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css';
+// Every component is a js function
+// only 1 root elm is allowed in a function
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
